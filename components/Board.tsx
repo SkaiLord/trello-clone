d8b74d1d5a57b86e08b794862879547118e45ced
SkaiLord <skailord007@gmail.com>
2023-06-06 14:12:01 +0530

    Board and TodoCard Added

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,18 +1,61 @@
 'use client'
 
+import { useBoardStore } from '@/store/BoardStore';
 import { useEffect } from 'react';
-import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import { DragDropContext, DropResult, Droppable } from 'react-beautiful-dnd';
+import Column from './Column';
 
 function Board() {
 
+  const [board, getBoard, setBoardState] = useBoardStore((state)=>[
+    state.board,
+    state.getBoard,
+    state.setBoardState,
+  ]);
+
     useEffect(()=>{
-        // getBoard();
-    }, [])
+      getBoard();
+    }, [getBoard])
+
+    const handleOnDragEnd = (result: DropResult) => {
+      const {source, destination, type} = result; 
+      console.log(source);
+      console.log(destination);
+      //check if user dragged card outside board
+      if(!destination) return;
+
+      //Column drag
+      if(type==='column'){
+        const entries = Array.from(board.columns.entries());
+        const [removed] = entries.splice(source.index, 1);
+        entries.splice(destination.index, 0, removed)
+        const rearrangedColumns = new Map(entries);
+        setBoardState({
+          ...board, columns: rearrangedColumns,
+        })
+      }
+
+      // card drag
+      if(type==='card')
+
+      //check if user dragged card to same position
+      if(destination.droppableId === source.droppableId && destination.index === source.index) return;
+    }
 
   return (
-    <DragDropContext>
+    <DragDropContext onDragEnd={handleOnDragEnd}>
         <Droppable droppableId='board' direction='horizontal' type='column'>
-
+          {(provided)=>
+            <div
+            className='grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto'
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >{
+              Array.from(board.columns.entries()).map(([id, column],index)=>(
+                <Column key={id} id={id} todos={column.todos} index={index}/>
+              ))
+            }</div>
+          }
         </Droppable>
     </DragDropContext>
   )
