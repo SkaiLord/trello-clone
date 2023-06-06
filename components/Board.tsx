8718f8393de1eb803885317d8dcb14899065c1ba
SkaiLord <skailord007@gmail.com>
2023-06-06 15:20:40 +0530

    Drag and Drop Functionality Added

--- components/Board.tsx
+++ components/Board.tsx
@@ -7,10 +7,11 @@ import Column from './Column';
 
 function Board() {
 
-  const [board, getBoard, setBoardState] = useBoardStore((state)=>[
+  const [board, getBoard, setBoardState, updateTodoInDB] = useBoardStore((state)=>[
     state.board,
     state.getBoard,
     state.setBoardState,
+    state.updateTodoInDB
   ]);
 
     useEffect(()=>{
@@ -24,7 +25,7 @@ function Board() {
       //check if user dragged card outside board
       if(!destination) return;
 
-      //Column drag
+      //Handle column drag
       if(type==='column'){
         const entries = Array.from(board.columns.entries());
         const [removed] = entries.splice(source.index, 1);
@@ -35,11 +36,56 @@ function Board() {
         })
       }
 
-      // card drag
-      if(type==='card')
+      // indexes are stored in numbers 0,1,2 etc instead of id's with DND library
+      const columns = Array.from(board.columns);
+      const startColIndex = columns[Number(source.droppableId)];
+      const endColIndex = columns[Number(destination.droppableId)];
+
+      const startCol: Column = {
+        id: startColIndex[0],
+        todos: startColIndex[1].todos,
+      }
+      const endCol: Column = {
+        id: endColIndex[0],
+        todos: endColIndex[1].todos,
+      }
+      if(!startCol || !endCol) return;
 
       //check if user dragged card to same position
-      if(destination.droppableId === source.droppableId && destination.index === source.index) return;
+      if(destination.index === source.index && startCol === endCol) return;
+
+      const newTodos = startCol.todos;
+      const [todoMoved] = newTodos.splice(source.index, 1);
+      
+      if(startCol.id === endCol.id){
+        //Same column task drag
+        newTodos.splice(destination.index, 0, todoMoved);
+        const newCol: Column={
+          id: startCol.id,
+          todos: newTodos
+        }
+        const newColumns = new Map(board.columns);
+        newColumns.set(startCol.id, newCol);
+
+        setBoardState({...board, columns: newColumns});
+
+      } else {
+        //dragging to another column
+        const endTodos = Array.from(endCol.todos);
+        endTodos.splice(destination.index, 0, todoMoved);
+        const newCol: Column={
+          id: startCol.id,
+          todos: newTodos
+        }
+        const newColumns = new Map(board.columns);
+        newColumns.set(startCol.id, newCol);
+        newColumns.set(endCol.id, {id:endCol.id, todos: endTodos,});
+
+        // Update in DB
+        updateTodoInDB(todoMoved, endCol.id);
+
+        setBoardState({...board, columns: newColumns});
+      }
     }
 
   return (
